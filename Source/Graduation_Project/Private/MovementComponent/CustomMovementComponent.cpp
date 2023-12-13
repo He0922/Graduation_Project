1484a4169cdcae05837de59e0e8f98374ea0881f
He0922 <HaominHe0@gmail.com>
2023-12-13 20:27:37 +0800

    Add CapsuleTrace And LineTrace In CustomMovementComponent

--- Source/Graduation_Project/Private/MovementComponent/CustomMovementComponent.cpp
+++ Source/Graduation_Project/Private/MovementComponent/CustomMovementComponent.cpp
@@ -2,4 +2,84 @@
 
 
 #include "MovementComponent/CustomMovementComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Explorer.h"
 
+
+
+void UCustomMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TraceClimbableSurfaces();
+	TraceFromEyeHeight(100.f);
+}
+
+
+
+#pragma region ClimbTrace
+TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMulitByObject(const FVector& Start, const FVector& End, bool showDebugShape)
+{
+	TArray<FHitResult> OutCapsuleTraceHitResult;
+
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,
+		Start,
+		End,
+		ClimbCapsuleTraceRadius,
+		ClimbCapsuleTraceHalfHeight,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		showDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutCapsuleTraceHitResult,
+		false
+	);
+	return OutCapsuleTraceHitResult;
+}
+
+
+FHitResult UCustomMovementComponent::DoLineTraceSingleByObject(const FVector& Start, const FVector& End, bool showDebugShape)
+{
+	FHitResult OutLineTraceHitResult;
+
+	UKismetSystemLibrary::LineTraceSingleForObjects(
+		this,
+		Start,
+		End,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		showDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutLineTraceHitResult,
+		false
+	);
+
+	return OutLineTraceHitResult;
+}
+#pragma endregion
+
+
+#pragma region ClimbCore
+void UCustomMovementComponent::TraceClimbableSurfaces()
+{
+	const FVector StartOffset = UpdatedComponent->GetForwardVector() * 30.f;
+	const FVector Start = UpdatedComponent->GetComponentLocation() + StartOffset;
+	const FVector End = Start + UpdatedComponent->GetForwardVector();
+	
+	DoCapsuleTraceMulitByObject(Start,End, true);
+}
+
+
+void UCustomMovementComponent::TraceFromEyeHeight(float TraceDistance, float TraceStartOffset)
+{
+	const FVector ComponentLocation = UpdatedComponent->GetComponentLocation();
+	const FVector EyeHeightOffset = UpdatedComponent->GetUpVector() * (CharacterOwner->BaseEyeHeight + TraceStartOffset);
+	
+	const FVector Start = ComponentLocation + EyeHeightOffset;
+	const FVector End = UpdatedComponent->GetForwardVector() + TraceDistance;
+
+	DoLineTraceSingleByObject(Start, End, true);
+
+}
+#pragma endregion
--- Source/Graduation_Project/Public/MovementComponent/CustomMovementComponent.h
+++ Source/Graduation_Project/Public/MovementComponent/CustomMovementComponent.h
@@ -13,5 +13,35 @@ UCLASS()
 class GRADUATION_PROJECT_API UCustomMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+public:
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 	
+#pragma region ClimbTrace
+public:
+	TArray<FHitResult> DoCapsuleTraceMulitByObject(const FVector& Start, const FVector& End, bool showDebugShape); 
+	FHitResult DoLineTraceSingleByObject(const FVector& Start, const FVector& End, bool showDebugShape);
+#pragma endregion
+
+
+#pragma region ClimbCore
+	void TraceClimbableSurfaces();
+
+	void TraceFromEyeHeight(float TraceDistance, float TraceStartOffset = 0.f);
+#pragma endregion
+
+
+#pragma region ClimbVariables
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	TArray<TEnumAsByte<EObjectTypeQuery> > ClimbableSurfaceTraceTypes;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceRadius = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceHalfHeight = 72.f;
+#pragma endregion
+
+
 };
