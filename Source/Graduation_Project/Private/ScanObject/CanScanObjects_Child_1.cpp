4798e79ba7fc18c17fcf3c0d6922a4b2bae1ff12
He0922 <HaominHe0@gmail.com>
2024-01-24 07:38:35 +0800

    Add ScanObjects Collision Event

--- Source/Graduation_Project/Private/ScanObject/CanScanObjects_Child_1.cpp
+++ Source/Graduation_Project/Private/ScanObject/CanScanObjects_Child_1.cpp
@@ -3,7 +3,7 @@
 
 #include "ScanObject/CanScanObjects_Child_1.h"
 #include "Components/BoxComponent.h"
-
+#include "Graduation_Project/DebugHelper.h"
 
 
 ACanScanObjects_Child_1::ACanScanObjects_Child_1(const FObjectInitializer& ObjectInitializer)
@@ -13,6 +13,17 @@ ACanScanObjects_Child_1::ACanScanObjects_Child_1(const FObjectInitializer& Objec
 	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
 	BoxCollision->SetBoxExtent(FVector(100.f, 100.f, 50.f));
 	BoxCollision->SetupAttachment(GetRootComponent());
+	// 设置是否生成碰撞事件(需要使用SetGenerateOverlapEvents来设置是否启用碰撞事件)
+	BoxCollision->SetGenerateOverlapEvents(true);
+
+	// 设置碰撞响应类型为Overlap
+	BoxCollision->SetCollisionResponseToAllChannels(ECR_Overlap);
+
+	// 绑定碰撞开始的事件函数
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ACanScanObjects_Child_1::OnOverlapBegin);
+
+	// 绑定碰撞结束的事件函数
+	BoxCollision->OnComponentEndOverlap.AddDynamic(this, &ACanScanObjects_Child_1::OnOverlapEnd);
 }
 
 
@@ -27,3 +38,17 @@ void ACanScanObjects_Child_1::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
+
+void ACanScanObjects_Child_1::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Debug::Print("Overlap Begin!");
+}
+
+void ACanScanObjects_Child_1::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Debug::Print("Overlap End!");
+}
+
+
+
+
--- Source/Graduation_Project/Public/ScanObject/CanScanObjects_Child_1.h
+++ Source/Graduation_Project/Public/ScanObject/CanScanObjects_Child_1.h
@@ -19,7 +19,7 @@ class GRADUATION_PROJECT_API ACanScanObjects_Child_1 : public ACanScanObjects
 
 public:
 	ACanScanObjects_Child_1(const FObjectInitializer& ObjectInitializer);
-	
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -31,4 +31,13 @@ public:
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Collision")
 	class UBoxComponent* BoxCollision;
+
+
+	// 声明碰撞开始的事件函数
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// 声明碰撞结束的事件函数
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
